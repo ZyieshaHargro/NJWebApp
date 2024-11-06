78799c3c28fcd40afd483feab0dea58d6f9a651f
Zyiesha Hargro <zyiesha.hargro@afs.com>
2024-11-06 13:06:04 -0500

    fixed the database stuff it runs again yayay

--- DoED/NewJoinerProj/demo/demo/src/main/resources/data.sql
+++ DoED/NewJoinerProj/demo/demo/src/main/resources/data.sql
@@ -1,4 +1,12 @@
-INSERT INTO TBL_EMPLOYEES (first_name, last_name, email) VALUES
-  ('Dan', 'Vega', 'danvega@gmail.com'),
-  ('Jen', 'Vega', 'jenvega@email.com'),
-  ('Bella', 'Vega', 'bellavega@gmail.com');
+INSERT INTO TBL_INSTRUMENTS (name, description, audioFilePath, imageFilePath, origin, history) VALUES
+  ('The Kora', 'The kora is a west african instrument ...', NULL, NULL, NULL, NULL);
+  ('The Thermin', 'The theremin is an electronic instrument that is played without physical contact', NULL, NULL, NULL, NULL),
+  ('The Guiro', 'Vega', NULL, NULL, NULL, NULL),
+  ('The Toha', 'Vega', NULL, NULL, NULL, NULL);
+--
+
+--  EXAMPLEEE
+--INSERT INTO TBL_EMPLOYEES (first_name, last_name, email) VALUES
+--    ('Dan', 'Vega', 'danvega@gmail.com'),
+--    ('Jen', 'Vega', 'jenvega@email.com'),
+--    ('Bella', 'Vega', 'bellavega@gmail.com');
